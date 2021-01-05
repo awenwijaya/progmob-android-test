eb7866f7ff097ec2791cafd275f1d8b0bcd1abde
awenwijaya <61014772+awenwijaya@users.noreply.github.com>
2021-01-06 02:33:00 +0800

    up gambar di request & detail

---
+++ app/src/main/java/klp18/praktikumprogmob/stt/DetailAcaraPenggunaActivity.java
@@ -0,0 +1,14 @@
+package klp18.praktikumprogmob.stt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailAcaraPenggunaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_acara_pengguna);
+    }
+}
